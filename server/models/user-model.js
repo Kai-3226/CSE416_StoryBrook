60d319360ac4d1ad147c51485b319133dc5cad66
JoonyoungLee0721 <vpfmsl@naver.com>
2022-04-01 14:22:00 -0400

    backend regarding works and quries

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -5,9 +5,32 @@ const ObjectId = Schema.Types.ObjectId
 const UserSchema = new Schema(
     {
         firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        email: { type: String, required: true },
-        passwordHash: { type: String, required: true }
+        lastName: { type: String, required: true},
+        passwordHash: { type: String, 
+                                    required: true },
+        email: { type: String, required: true},
+        friends: { type: [String], require: true},
+        following: { type: [String], require: true},
+        follower: { type: [String], require: true},
+        message: { type: {"useId":  String,
+                                     "unread": Number, 
+                                     "content": [{"isYou": Boolean,   
+                                                        "message": String}]
+                                    },
+                           require: true },      
+        works: { type: [{"works": String}], 
+                     required: true},
+        comicLibrary: { type: [{"libraryId": String}], 
+                                required: true},
+        like: { type: [String], required: true},
+        dislike: { type: [String], required: true},
+        alarm: { type: [Object], required: true},
+        profile: { type:{"age": Number, 
+                              "gender": Number,
+                              "userName":String ,
+                              "myStatement": String,
+                              "icon": Object},
+                      required: true}
     },
     { timestamps: true },
 )
---
+++ server/routes/works-router.js
@@ -0,0 +1,18 @@
+const auth = require('../auth')
+const express = require('express')
+const WorkController = require('../controllers/work-controller')
+const UserController = require('../controllers/user-controller')
+const router = express.Router()
+
+router.post('/work', auth.verify, WorkController.createWork)
+router.put('/work/:id', auth.verify, WorkController.updateWork)
+router.delete('/work/:id', auth.verify, WorkController.deleteWork)
+router.get('/work/:id', WorkController.getWorkById)
+router.get('/works', WorkController.getWorks)
+router.get('/top5listpairs', WorkController.getWorks)
+
+router.post('/register', UserController.registerUser)
+router.post('/login',UserController.loginUser)
+router.get('/logout',UserController.logoutUser)
+router.get('/loggedIn', UserController.getLoggedIn)
+module.exports = router
