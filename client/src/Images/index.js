1dd3b28d7f1369b407264fb74c177e2880c3b678
Xiyang Hu <xiyang.hu@stonybrook.edu>
2022-04-18 14:01:29 -0400

    splashscreen

--- client/src/Images/index.js
+++ client/src/Images/index.js
@@ -1,8 +1,7 @@
-import check from '../Images/check.png'
 import Comics from '../Images/Comics.png'
 import Logo from '../Images/Logo.png'
-import splashScreen from '../Images/splashScreen.png'
 import storytelling from '../Images/storytelling.png'
+import splashScreen from '../Images/splashScreen.png'
 const images = {
     Comics, Logo, splashScreen, storytelling
 }
