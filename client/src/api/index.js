3051c4e71d04da9bcadb97e1ab5a21dacdce5398
Xuankai <hxkkai3226@gmail.com>
2022-04-20 16:27:22 -0400

    fix 5

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -13,7 +13,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'http://localhost:4000',    //'https://storybrook.herokuapp.com/api'
+    baseURL: 'https://storybrook.herokuapp.com/api',    
 })
 
 // THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
