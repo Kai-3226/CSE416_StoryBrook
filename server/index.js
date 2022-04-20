3fb1c4083e4b43c09668b4b08a040d21f666ef51
hua lin <harryhualin@gmail.com>
2022-04-20 02:29:38 -0400

    55

--- server/index.js
+++ server/index.js
@@ -29,6 +29,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 // Heroku Deploy
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 if (process.env.NODE_ENV === 'production') 
     app.use(express.static('client/build'));
