24daf485c3552c1dcd9141378fe237b864d0d757
hua lin <harryhualin@gmail.com>
2022-04-21 00:28:25 -0400

    ADF

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production')
     {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => { 
-        res.sendFile(path.join(__dirname, 'client/build/index.html'));
+        res.sendFile('/app/client/build/index.html');
         })
     }
 
