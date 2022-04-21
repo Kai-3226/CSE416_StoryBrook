1217c00e0fdfd22d725c46e309aad41e461157c6
hua lin <harryhualin@gmail.com>
2022-04-21 14:25:40 -0400

    dafaf

--- client/src/components/MypageProfile.js
+++ client/src/components/MypageProfile.js
@@ -4,41 +4,56 @@ import Button from '@mui/material/Button';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import { useContext, useState } from 'react';
 import { GlobalStoreContext } from '../store';
+import AuthContext from '../auth';
+
 export default function Profile(){
     const store=useContext(GlobalStoreContext);
     const [ editActive, setEditActive ] = useState(false);
     const [ text, setText ] = useState("");
+    const { auth } = useContext(AuthContext);
+
+    function getAccountMenu(loggedIn) {
+        if(loggedIn){
+            let lastname=auth.user.lastName.substring(0,1).toUpperCase();
+            let firstname=auth.user.firstName.substring(0,1).toUpperCase();
+            return(
+                <Button sx={{color:"e0e0e0"}}
+                variant="outlined">
+                    {firstname+lastname}
+                </Button>
+            );
+        }
+        return <AccountCircle />;
+    }
     return (
         <div>
             <Box id="profilePage">
                 <Box id="profile-head" >
-                    <AccountCircle id="profile=ac"></AccountCircle>
-                    <Box id="profile-mg">You can upload your image here</Box>
-                    <Box id="profile-upload"><Button sx={{color: 'black',backgroundColor:"#c7ca3d"}}>upload image</Button></Box>
+                    {/* <AccountCircle id="profile=ac"></AccountCircle> */}
+                    { getAccountMenu(auth.loggedIn) }
+                    {/* <Box id="profile-mg">You can upload your image here</Box>
+                    <Box id="profile-upload"><Button sx={{color: 'black',backgroundColor:"#c7ca3d"}}>upload image</Button></Box> */}
                 </Box>
                 <Box id="profile-setting">
                     <Box>Main Settings</Box>
-                    <Box>ChangePassword
-                        <Box><Textfield></Textfield><Button sx={{backgroundColor: '#c4c4c4',
-  borderColor: '#c4c4c4',color:"black"}}>Edit</Button></Box>
-                    </Box>
-                    <Box display="flex">
+                    <Box display="flex" sx={{paddingTop:'5%'}}>
                     <Box id="profile-name">User Name
-                        <Box><Textfield></Textfield></Box>
+                        <Box><Textfield defaultValue={auth.user.firstName+auth.user.lastName}></Textfield></Box>
                     </Box>
                     <Box id="profile-age">Age
-                        <Box><Textfield></Textfield></Box>
+                        <Box><Textfield defaultValue={auth.user.age}></Textfield></Box>
                     </Box>
                     <Box id="profile-gender">Gender
-                        <Box><Textfield></Textfield></Box>
+                        <Box><Textfield defaultValue={auth.user.gender}></Textfield></Box>
                     </Box>
                     </Box>
-                    <Box id="profile-email">User Email
-                        <Box>user email will be displyaed here.</Box>
-                    </Box>  
-                    <Box id="profile-statement">My Statement
-                        <Box><Textfield></Textfield></Box>
-                    </Box>
+                    <Box id="profile-email" sx={{paddingTop:'5%'}}>User Email
+                        <Box defaultValue={auth.user.email}>user email will be displyaed here.</Box>
+                    </Box> 
+                    <Box>ChangePassword
+                        <Box><Textfield></Textfield><Button sx={{backgroundColor: '#c4c4c4',
+                        borderColor: '#c4c4c4',color:"black"}}>Edit</Button></Box>
+                    </Box> 
                 </Box>
                 <Box id="profile-button"></Box>
                     <Box>
