feef90e7080d0995be929fa6ddce4883e0c9ebbc
hua lin <harryhualin@gmail.com>
2022-04-21 19:33:59 -0400

    adfadsf

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -13,7 +13,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'https://storybrook.herokuapp.com/api',    
+    baseURL: 'https://sbrook.herokuapp.com/api',    
 })
 
 // THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
