5f93c03337a31ea40308c0ad51ac5264b925f4ac
hua lin <harryhualin@gmail.com>
2022-04-21 01:12:11 -0400

    ADFADF

--- server/utils/email/sendEmail.js
+++ server/utils/email/sendEmail.js
@@ -6,37 +6,44 @@ const path = require("path");
 
 const sendEmail = async (email, subject, payload, template) => {
  
-    // Create a SMTP transporter object
-    let transporter = nodemailer.createTransport({
-      host: "hotmail",
-      auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
-      }
-  });
-  
-    ////Message object
-    const source = fs.readFileSync(path.join(__dirname, template), "utf8");
-    const compiledTemplate = handlebars.compile(source);
-    const options = () => {
-      return {
-        from: process.env.EMAIL_USERNAME,
-        to: email,
-        subject: subject,
-        html: compiledTemplate(payload),
-      };
-    };
-
-    transporter.sendMail(options(), (err, info) => {
-      if (err) {
-        console.log('Error occurred. ' + err.message);
-        return process.exit(1);
+   // Create a SMTP transporter object
+   let transporter = nodemailer.createTransport({
+    host: "hotmail",
+    port: 587,
+    secure: false,
+    tls: {
+      ciphers:'SSLv3'
+      },
+    requireTLS:true,//this parameter solved problem for me
+    auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
     }
+});
 
-    
-        // Preview only available when sending through an Ethereal account
-       
-    })
+////Message object
+const source = fs.readFileSync(path.join(__dirname, template), "utf8");
+const compiledTemplate = handlebars.compile(source);
+const options = () => {
+  return {
+    from: process.env.EMAIL_USERNAME,
+    to: email,
+    subject: subject,
+    html: compiledTemplate(payload),
+  };
+};
+
+transporter.sendMail(options(), (err, info) => {
+    if (err) {
+        console.log('Error occurred. ' + err.message);
+        console.log(process.env.EMAIL_USERNAME);
+        return false;
+    }
+    else return true;
+    // Preview only available when sending through an Ethereal account
+   
+})
+
 
 };
 
@@ -44,10 +51,10 @@ const sendEmail = async (email, subject, payload, template) => {
 /*
 Example:
 sendEmail(
-  "youremail@gmail.com,
-  "Email subject",
-  { name: "Eze" },
-  "./templates/layouts/main.handlebars"
+"youremail@gmail.com,
+"Email subject",
+{ name: "Eze" },
+"./templates/layouts/main.handlebars"
 );
 */
 
