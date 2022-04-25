e358623b700ccc9df7017be7e7b34199cf6e4a9e
hua lin <harryhualin@gmail.com>
2022-04-24 20:53:02 -0400

    sd

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 // THESE ARE NODE APIs WE WISH TO USE
 const express = require('express')
 const cors = require('cors')
-const dotenv = require('dotenv')
+//const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
-const path=require('path')
+//const path=require('path')
 // CREATE OUR SERVER
  //dotenv.config();
 const PORT = process.env.PORT || 4000;
