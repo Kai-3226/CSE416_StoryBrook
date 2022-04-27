cbfafed47a08891e6fbcec2d052c15b48184b159
Xuankai <hxkkai3226@gmail.com>
2022-04-27 16:44:35 -0400

    kk

--- client/src/components/ViewScreen.js
+++ client/src/components/ViewScreen.js
@@ -58,20 +58,21 @@ const ViewScreen = () => {
 
     let work = "";
     if (store && store.workList) {
-        // list = store.workList;
-        console.log(store.workList);
-        // const rows = list.reduce(function (rows, key, index) { 
-        //     return (index % 4 == 0 ? rows.push([key]) 
-        //       : rows[rows.length-1].push(key)) && rows;
-        //   }, []);
-        // console.log(rows);
-
-
-        let rows = [];
-        for (var i = 0, end = store.workList.length / 4; i < end; ++i){
-            rows.push(store.workList.slice(i * 4, (i + 1) * 4));
-        }
+        list = store.workList;
+        console.log(list);
+        list = list.filter(item => item.published["publish"] === true);
+        const rows = list.reduce(function (rows, key, index) { 
+            return (index % 4 == 0 ? rows.push([key]) 
+              : rows[rows.length-1].push(key)) && rows;
+          }, []);
         console.log(rows);
+
+
+        // let rows = [];
+        // for (var i = 0, end = store.workList.length / 4; i < end; ++i){
+        //     rows.push(store.workList.slice(i * 4, (i + 1) * 4));
+        // }
+        // console.log(rows);
         work = 
             rows.map((row) => (
                 <Box sx = {{display:'flex',position:'relative'}}>
