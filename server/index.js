30753c439f457bd443bcb027654d8350f0377df7
hua lin <harryhualin@gmail.com>
2022-04-28 16:15:15 -0400

    1

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-    //origin: ["https://sbrook.herokuapp.com"],
-    origin: ["http://localhost:3000"],
+    origin: ["https://sbrook.herokuapp.com"],
+    //origin: ["http://localhost:3000"],
     //origin: [https://storybrook.herokuapp.com],
     credentials: true
 }))
