033eb62bda593f5c7736c2eee5fb62c56e157604
JoonyoungLee0721 <vpfmsl@naver.com>
2022-05-13 03:05:37 -0400

    unfollow in mypage working

--- Desktop/CSE416/CSE416_StoryBrook/client/src/components/Card.js
+++ Desktop/CSE416/CSE416_StoryBrook/client/src/components/Card.js
@@ -13,22 +13,19 @@ export default function Card(props) {
     const { store } = useContext(GlobalStoreContext);
     const { auth } = useContext(AuthContext)
 
-    function handleMessage(){
-
-    }
-    function handleDelete(){
-        
+    function handleDelete(event,target){
+        event.stopPropagation();
+        auth.unfollowAuthor(target);
     }
     let cardElement =""
     const prop = props.props;
     if (store.mode=="followings"){
-        let target = ""
         cardElement =
             <ListItem sx={{bgcolor: "red",border: "1px solid black"}}>
                     <Box sx={{ p: 1, flexGrow: 1 }}>{prop.email}</Box>
                     <Box sx={{ p: 1 }}>
                         <IconButton onClick={(event) => {
-                            handleDelete(event, target)
+                            handleDelete(event, prop._id)
                         }} aria-label='delete'>
                             <DeleteIcon style={{fontSize:'48pt'}} />
                         </IconButton>
